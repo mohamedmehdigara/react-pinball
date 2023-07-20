04aaa97763544040e0a388b86a769accfc461e7c
mohamedmehdigara <garamehdi57@yahoo.com>
2023-07-20 18:08:25 +0100

    added files

--- src/Pinball.js
+++ src/Pinball.js
@@ -23,7 +23,7 @@ const Flipper = styled.div`
   position: absolute;
   bottom: 20px;
   left: ${(props) => (props.right ? 'calc(100% - 120px)' : '0')};
-  transform: ${(props) => (props.up ? 'rotate(45deg)' : 'rotate(0)')};
+  transform: ${(props) => (props.up ? 'rotate(-45deg)' : 'rotate(0)')};
   transform-origin: ${(props) => (props.right ? '100% 100%' : '0% 100%')};
   transition: transform 0.2s ease;
 `;
@@ -39,11 +39,48 @@ const Ball = styled.div`
   transition: top 0.2s ease;
 `;
 
+const Wall = styled.div`
+  width: 30px;
+  height: 300px;
+  background-color: #999;
+  position: absolute;
+  top: 150px;
+  left: ${(props) => (props.right ? 'calc(100% - 30px)' : '0')};
+`;
+
+const SideWall = styled.div`
+  width: 10px;
+  height: 600px;
+  background-color: #333;
+  position: absolute;
+  top: 0;
+  left: ${(props) => (props.right ? 'calc(100% - 10px)' : '0')};
+`;
+
+const CenterBumper = styled.div`
+  width: 100px;
+  height: 20px;
+  background-color: #555;
+  position: absolute;
+  top: 250px;
+  left: calc(50% - 50px);
+`;
+
+const Tube = styled.div`
+  width: 100px;
+  height: 200px;
+  background-color: #777;
+  position: absolute;
+  top: 100px;
+  left: ${(props) => (props.right ? 'calc(100% - 100px)' : '0')};
+`;
+
 function Pinball() {
   const [leftFlipperUp, setLeftFlipperUp] = useState(false);
   const [rightFlipperUp, setRightFlipperUp] = useState(false);
   const [ballPosition, setBallPosition] = useState({ x: 400, y: 580 });
   const [gameOver, setGameOver] = useState(false);
+  const [score, setScore] = useState(0);
 
   const handleKeyDown = (e) => {
     if (e.key === 'ArrowLeft') {
@@ -76,6 +113,23 @@ function Pinball() {
 
       const newPosition = { x: ballPosition.x, y: ballPosition.y + (leftFlipperUp || rightFlipperUp ? -5 : 5) };
 
+      // Check for collision with walls
+      if (newPosition.x <= 30 || newPosition.x >= 770) {
+        newPosition.x = ballPosition.x; // Prevent moving outside the game area
+      }
+
+      // Check for collision with flippers
+      if (newPosition.y >= 570 && newPosition.x >= 200 && newPosition.x <= 600) {
+        newPosition.y = 570;
+      }
+
+      // Check for collision with center bumper
+      if (newPosition.y >= 230 && newPosition.y <= 270 && newPosition.x >= 350 && newPosition.x <= 450) {
+        setScore(score + 100);
+        newPosition.y = 230;
+      }
+
+      // Check for game over
       if (newPosition.y > 580) {
         setGameOver(true);
         return;
@@ -88,15 +142,33 @@ function Pinball() {
     return () => {
       clearInterval(interval);
     };
-  }, [ballPosition, leftFlipperUp, rightFlipperUp, gameOver]);
+  }, [ballPosition, leftFlipperUp, rightFlipperUp, gameOver, score]);
+
+  useEffect(() => {
+    if (gameOver) {
+      setTimeout(() => {
+        setGameOver(false);
+        setBallPosition({ x: 400, y: 580 });
+        setScore(0);
+      }, 3000); // 3 seconds delay before resetting the game
+    }
+  }, [gameOver]);
 
   return (
     <Container>
       <PinballGame>
         <Flipper up={leftFlipperUp} />
         <Flipper up={rightFlipperUp} right />
+        <Wall />
+        <Wall right />
+        <SideWall />
+        <SideWall right />
+        <CenterBumper />
+        <Tube />
+        <Tube right />
         <Ball position={ballPosition} />
         {gameOver && <h1>Game Over</h1>}
+        <h2>Score: {score}</h2>
       </PinballGame>
     </Container>
   );
