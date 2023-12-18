2a43e64189f261908d0c1d16af9ecf7aeb10892a
mohamedmehdigara <garamehdi57@yahoo.com>
2023-12-18 15:50:10 +0100

    edited files

--- src/components/Tube.js
+++ src/components/Tube.js
@@ -1,55 +1,35 @@
+// Tube.js
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
 
-// Define a pulse animation
 const pulse = keyframes`
   0% { transform: scale(1); }
   50% { transform: scale(1.2); }
   100% { transform: scale(1); }
 `;
 
-const Tube = ({ type }) => {
-  const getTubePosition = () => {
-    switch (type) {
-      case 'top':
-        return { top: '50px', left: '300px' };
-      case 'middle':
-        return { top: '200px', left: '600px' };
-      case 'bottom':
-        return { top: '400px', left: '300px' };
-      default:
-        return {};
-    }
-  };
-
-  return (
-    <TubeContainer type={type} style={{ ...getTubePosition() }}>
-    <svg
-      width="100"
-      height="100"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      {/* Your SVG content here */}
-      <circle cx="50" cy="50" r="45" fill="#fff" />
-    </svg>
-  </TubeContainer>
-  );
-};
-
 const TubeContainer = styled.div`
-  width: 100px;
-  height: 100px;
-  background-color: #777;
   position: absolute;
-  border-radius: 50%;
   cursor: pointer;
-  transition: background-color 0.3s ease, transform 0.3s ease; /* Smooth transitions for color and scale */
+  ${(props) => {
+    switch (props.type) {
+      case 'vertical':
+        return 'top: 50px; right: 50px;'; // Adjust positioning based on your design
+      default:
+        return '';
+    }
+  }}
+  transition: background-color 0.3s ease, transform 0.3s ease;
 
   &:hover {
-    background-color: #888; /* Change color on hover */
-    transform: scale(1.1); /* Scale up on hover */
-    animation: ${pulse} 0.5s ease infinite; /* Apply the pulse animation on hover */
+    background-color: #888;
+    transform: scale(1.1);
+    animation: ${pulse} 0.5s ease infinite;
   }
 `;
+
+const Tube = ({ type }) => {
+  return <TubeContainer type={type} />;
+};
 
 export default Tube;
